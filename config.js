0d99c9facab73586739180b6c2f89964906a9d8b
Yihui Xiong <xiongyihui3@gmail.com>
2016-12-20 16:14:55 +0800

    fix config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 self.$config = {
-  title: 'ReSpeaker Wiki'
+  title: 'ReSpeaker Wiki',
+  repo: 'respeaker/docs',
+  url: 'https://respeaker.io/docs',
+  'edit-link': 'https://github.com/respeaker/docs/blob/master',
   nav: [
     // 首页
     {title: 'Home', path: '/'}
   ]
-  repo: 'respeaker/docs',
-  url: 'https://respeaker.io/docs',
-  'edit-link': 'https://github.com/respeaker/docs/blob/master',
 }
